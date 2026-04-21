889e017a577dd5d4a6c7bb8f7d2561d96a58457d
Jayesh-019 <shuklajayesh9999@gmail.com>
2026-04-21 23:57:18 +0530

    Backend updated and requirements.txt added

---
+++ backend/utils/video_processor.py
@@ -0,0 +1,14 @@
+import cv2
+
+class VideoProcessor:
+    def __init__(self, source=0):
+        self.cap = cv2.VideoCapture(source)
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        return frame
+
+    def release(self):
+        self.cap.release()
